422f8a56fa50590176ee6782983d2bf5d737deee
Tii <mail@tii.one>
2024-05-23 15:36:38 +0200

    Run command cleanly

--- app/Services/ZXParserService.php
+++ app/Services/ZXParserService.php
@@ -20,10 +20,14 @@ class ZXParserService
         $corePath = base_path('bin/zebra-crossing/core-3.5.3.jar');
         $jCommanderPath = base_path('bin/zebra-crossing/jcommander-1.83.jar');
 
-        $command = "$javaPath -cp $javaSEPath:$corePath:$jCommanderPath com.google.zxing.client.j2se.CommandLineRunner $imagePath --possible_formats AZTEC";
-
         // Run java command
-        $result = Process::run($command);
+        $result = Process::run([
+            $javaPath,
+            '-cp', "$javaSEPath:$corePath:$jCommanderPath",
+            'com.google.zxing.client.j2se.CommandLineRunner',
+            $imagePath,
+            '--possible_formats', 'AZTEC',
+        ]);
         $output = $result->output();
 
         if (str_contains($output, 'No barcode found')) {
